ab1209cecee30709886832a2366915976fb60cec
zero-plusplus <git@zero-plusplus.com>
2021-10-30 18:00:48 +0900

    Add IncludeInliner

---
+++ src/modules/IncludeInliner.ts
@@ -0,0 +1,70 @@
+import * as path from 'path';
+import { readFileSync } from 'fs';
+import { AhkVersion } from './AhkVersion';
+import { IncludePathResolver, SupportVariables, defaultSupportVariables } from './IncludePathResolver';
+import { addBom, stripBom, toCrlf } from '../util/string';
+import { IncludeInfo } from './IncludePathExtractor';
+import { isDirExist, isPathExist } from '../util/file';
+import { ImplicitFunctionPathExtractor } from './ImplicitFunctionPathExtractor';
+
+export class IncludeInliner {
+  public readonly version: AhkVersion;
+  private readonly resolver: IncludePathResolver;
+  private readonly extractor: ImplicitFunctionPathExtractor;
+  constructor(version: AhkVersion) {
+    this.version = version;
+    this.resolver = new IncludePathResolver(version);
+    this.extractor = new ImplicitFunctionPathExtractor(version);
+  }
+  /**
+   * @param libraryTypes
+   */
+  public exec(rootPath: string, overwriteVariables?: Partial<SupportVariables>, pathReplacer: (includeInfo: IncludeInfo) => string = (includeInfo): string => includeInfo.path): string {
+    const inline = (filePath: string, overwriteVariables?: Partial<SupportVariables>): string => {
+      const variables = {
+        ...defaultSupportVariables,
+        ...overwriteVariables,
+        A_LineFile: path.resolve(rootPath),
+      };
+      if (!variables.A_ScriptDir) {
+        variables.A_ScriptDir = path.dirname(rootPath);
+      }
+      if (!variables.A_WorkingDir) {
+        variables.A_WorkingDir = variables.A_ScriptDir;
+      }
+
+      const source = stripBom(readFileSync(filePath, 'utf-8'));
+      return source.replace(/^(?<!;)(?<indent>\s*)#Include(?:|(?<isAgainMode>Again))\s+(?:|(?<isOptional>[*]i)\s+)(?:(?<includePath>[^*\s\r\n<>]+)|<(?<libraryPath>[^*\s\r\n<>]+)>)[^\r\n\S]*(?<linebreak>\r\n|\n)?/gium, (...params) => {
+        const { isAgainMode, isOptional, includePath, libraryPath, linebreak } = params[params.length - 1]!;
+        const includeInfo: IncludeInfo = {
+          isAgainMode: Boolean(isAgainMode),
+          isLibraryMode: Boolean(libraryPath),
+          isOptional: Boolean(isOptional),
+          path: libraryPath || includePath,
+        };
+
+        const resolvedPath = this.resolver.resolve(includeInfo.isLibraryMode ? `<${includeInfo.path}>` : includeInfo.path, variables);
+        if (!resolvedPath) {
+          throw Error(`File not found => "${includeInfo.path}"`);
+        }
+
+        if (isDirExist(resolvedPath)) {
+          variables.PseudoVariable_IncludeBaseDir = resolvedPath;
+          return '';
+        }
+        if (isPathExist(resolvedPath)) {
+          const inlined = inline(resolvedPath, variables);
+          return linebreak ? `${inlined}${String(linebreak)}` : inlined;
+        }
+        throw Error(`File not found => "${includeInfo.path}"`);
+      });
+    };
+
+    const inlinedSource = inline(rootPath, overwriteVariables);
+    const implictFunctions = this.extractor.extract(rootPath).map((filePath) => {
+      return stripBom(readFileSync(filePath, 'utf-8'));
+    }).join('\r\n');
+    const newSource = implictFunctions ? `${implictFunctions}\r\n${inlinedSource}` : inlinedSource;
+    return toCrlf(addBom(newSource));
+  }
+}
---
+++ src/util/string.ts
@@ -0,0 +1,61 @@
+
+export const indent = (str: string, indent: string): string => {
+  return `${indent}${str.replace(/(\r\n|\n)/gu, `$1${indent}`)}`;
+};
+export const getIndent = (str: string): string => {
+  const indentMatch = str.match(/^(\s*)/u);
+  return indentMatch ? indentMatch[1] : '';
+};
+export const repeat = (str: string, count: number): string => {
+  let repeated = '';
+  for (let i = 0; i < count; i++) {
+    repeated += str;
+  }
+  return repeated;
+};
+export const trimMargin = (template: TemplateStringsArray, ...substitutions: any[]): string => {
+  const _trimMargin = (string: string, separator = '\\|'): string => {
+    // Remove first spaces
+    const lines = string.replace(/^(\r\n|\r|\n)/u, '').split('\n');
+
+    const baseIndentSize = 0 < lines.length ? getIndent(lines[0]).length : 0;
+    let trimmed = '';
+    for (let i = 0; i < lines.length; i++) {
+      let line = lines[i];
+      const currentIndentSize = getIndent(line).length;
+
+      // Ignore last line spaces
+      if (i + 1 === lines.length && currentIndentSize === line.length) {
+        continue;
+      }
+
+      if (currentIndentSize < baseIndentSize) {
+        line = `${repeat(' ', baseIndentSize)}${line}`;
+      }
+
+      const trimmedMargin = line.replace(new RegExp(`(^\\s*${separator}|^\\s{0,${baseIndentSize}})`, 'u'), '');
+      trimmed += `${trimmedMargin}\n`;
+    }
+    trimmed = trimmed
+      .replace(/\n$/u, '')
+      .replace(/(?<=\n)\\s+$/u, '')
+      .replace(/\\\\/gu, '\\');
+
+    return trimmed;
+  };
+
+  const templated = String.raw(template, ...substitutions)
+    .replace(/\\`/gu, '`');
+  return _trimMargin(templated);
+};
+
+const bom = '\uFEFF';
+export const addBom = (str: string): string => {
+  return bom + str;
+};
+export const stripBom = (str: string): string => {
+  return str.replace(new RegExp(`^${bom}`, 'u'), '');
+};
+export const toCrlf = (str: string): string => {
+  return str.replace(/(?<!\r)\n/gu, '\r\n');
+};
