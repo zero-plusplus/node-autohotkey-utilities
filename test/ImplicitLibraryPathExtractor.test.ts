e351d345fd5808f787c45ce21e960da7322cd2ca
zero-plusplus <git@zero-plusplus.com>
2022-02-08 22:17:59 +0900

    Update test

--- test/ImplicitLibraryPathExtractor.test.ts
+++ test/ImplicitLibraryPathExtractor.test.ts
@@ -1,9 +1,10 @@
 import * as path from 'path';
-import { AhkVersion, ImplicitLibraryPathExtractor } from '../src';
+import { AhkVersion, ImplicitLibraryPathExtractor, IncludePathExtractor } from '../src';
 
 const v1_1 = new AhkVersion('1.1.33.10');
 // const v2_0_b2 = new AhkVersion('2.0-beta.2');
 describe('v1', () => {
+  const includeExtractor = new IncludePathExtractor(v1_1);
   const extractor = new ImplicitLibraryPathExtractor(v1_1);
   const rootDirPath = path.resolve(__dirname, 'sample', 'ahk');
   const rootPath = path.resolve(rootDirPath, 'main.ahk');
@@ -14,5 +15,8 @@ describe('v1', () => {
       path.resolve(rootDirPath, 'lib', 'ImplicitFunction.ahk'),
       path.resolve(rootDirPath, 'lib', 'LocalLibFunction.ahk'),
     ]);
+
+    const includePathList = includeExtractor.extract(rootPath, { A_AhkPath: `${rootDirPath}/stdlib/dummy.exe` });
+    expect(extractor.extract([ rootPath, ...includePathList ], { A_AhkPath: `${rootDirPath}/stdlib/dummy.exe` })).toEqual(actual);
   });
 });
